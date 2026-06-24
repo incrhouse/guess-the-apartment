eae74ec6f5c8fc86dfed45c004e5aa99e58e48e7
Cleveland Robinson <cleveland@incrhouse.com>
2026-06-24 10:04:17 -0500

    add adsense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8217135306638009"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
